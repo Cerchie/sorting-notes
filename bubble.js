a63f8fbbd29574804223142d44a037a337a268c9
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-25 10:28:53 -0700

    completes assignment

--- bubble.js
+++ bubble.js
@@ -1,3 +1,37 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        let swapped = false;
+        for (let j = 0; j < arr.length -i; j++){
+            count ++;
+            console.log(count)
+            if(arr[j] > arr[j+1]){
 
+                let temp = arr[j];
+                arr[j] = arr[j +1]
+                arr[j+1]=temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    } 
+
+    return arr;
+}
+
+// function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
+  
+//     for (let i = arr.length; i > 0; i--) {
+//       for (let j = 0; j < i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           swap(arr, j, j + 1);
+//         }
+//       }
+//     }
+  
+//     return arr;
+//   }
 module.exports = bubbleSort;
